033f7cf170f59d08d74bd49db14d246c6904de65
eve seni <senieve@gmail.com>
2024-06-07 09:15:03 +0200

    sidebar active links

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+"use client";
 import { sidebarLinks } from "@/app/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <section className="sticky left-0 top-0 flex w-fit flex-col justify-between border-none bg-black-1 pt-8 text-white-1 max-md:hidden lg:w-[270px] lg:pl-8">
       <nav className="flex flex-col gap-6">
@@ -18,8 +24,17 @@ const LeftSidebar = () => {
         </Link>
 
         {sidebarLinks.map(({ route, label, imgURL }) => {
+          //for active route hover selector
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
           return (
-            <Link href={route} key={label} className="gap-3 flex ">
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+              )}>
               <Image src={imgURL} alt={label} width={24} height={24} />
               <p>{label}</p>
             </Link>
