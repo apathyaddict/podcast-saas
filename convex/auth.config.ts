019a2d63449494aa3e17c33ceac6d7232ef4f97c
eve seni <senieve@gmail.com>
2024-06-09 19:30:28 +0200

    clerk auth set up

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://popular-hedgehog-58.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
