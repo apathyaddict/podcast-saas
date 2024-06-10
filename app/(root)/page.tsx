564b76e731c9c117f4dae693a9c82121e853ed15
eve seni <senieve@gmail.com>
2024-06-10 16:55:35 +0200

    input form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,15 +7,15 @@ import { podcastData } from "../constants";
 import PodcastCard from "@/components/PodcastCard";
 
 const Home = () => {
-  const tasks = useQuery(api.tasks.get);
+  // const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
 
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+        {/* <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
           {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-        </main>
+        </main> */}
 
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {podcastData.map(({ id, imgURL, description, title }) => (
